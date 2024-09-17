68e980783a11e2e911df7a026cd6470c6085bbc1
raquelfernanda490 <raquelfernanda490@gmail.com>
2024-09-17 19:05:55 -0300

    atualização js no menu e sua visibilidade

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-var hamburguer = document.getElementById("hamburguer");
-var itens_menu = document.getElementById("itens_menu");
+let hamburguer = document.getElementById('hamburguer');
+let itens_menu = document.getElementById('itens_menu');
 
-hamburguer.addEventListener("click", aparecer);
+hamburguer.addEventListener('click', aparecer);
 
 function aparecer() {
-  if (itens_menu.style.display == "block") {
-    itens_menu.classList.remove("show");
+  if (itens_menu.style.display == 'block') {
+    itens_menu.classList.remove('show');
   } else {
-    itens_menu.classList.toggle("show");
+    itens_menu.classList.toggle('show');
+
   }
+
+    
+}
+
+
+function scroll() {
+    document.getElementById('scroll').scrollIntoView({behavior:'smooth'});
+  }
+  
+function scrollPadding(id) {
+  const padding = document.getElementById(id);
+
+  if (itens_menu.classList.contains('show')) {
+    padding.style.paddingTop = '50px';
+    itens_menu.classList.toggle('show')
+}else{
+  padding.style.paddingTop = 0;
+}
 }
